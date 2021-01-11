fe7ba479410c3550acda352a8d1f53f05b753adc
Joe Bustamante <josephmbustamante@gmail.com>
2021-01-11 13:41:38 -0500

    Add a max lines remembered variable, and delete any lines over that amount (part #5)

--- Game.gd
+++ Game.gd
@@ -3,6 +3,8 @@ extends Control
 
 const InputResponse = preload("res://InputResponse.tscn")
 
+export (int) var max_lines_remembered := 30
+
 var max_scroll_length := 0
 
 onready var history_rows = $Background/MarginContainer/Rows/GameInfo/Scroll/HistoryRows
@@ -22,6 +24,14 @@ func handle_scrollbar_changed():
 
 
 func _on_Input_text_entered(new_text: String) -> void:
+	if new_text.empty():
+		return
+
 	var input_response = InputResponse.instance()
 	input_response.set_text(new_text, "This is where a response would go.")
 	history_rows.add_child(input_response)
+
+	if history_rows.get_child_count() > max_lines_remembered:
+		var rows_to_forget = history_rows.get_child_count() - max_lines_remembered
+		for i in range(rows_to_forget):
+			history_rows.get_child(i).queue_free()
