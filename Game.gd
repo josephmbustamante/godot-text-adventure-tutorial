b96b6b76b05912d603951acf84c868551231f79d
Joe Bustamante <josephmbustamante@gmail.com>
2021-01-11 13:23:17 -0500

    Display user input and scroll to bottom when new input is entered (part #4)

--- Game.gd
+++ Game.gd
@@ -3,7 +3,22 @@ extends Control
 
 const InputResponse = preload("res://InputResponse.tscn")
 
-onready var history_rows = $Background/MarginContainer/Rows/GameInfo/ScrollContainer/HistoryRows
+var max_scroll_length := 0
+
+onready var history_rows = $Background/MarginContainer/Rows/GameInfo/Scroll/HistoryRows
+onready var scroll = $Background/MarginContainer/Rows/GameInfo/Scroll
+onready var scrollbar = scroll.get_v_scrollbar()
+
+
+func _ready() -> void:
+	scrollbar.connect("changed", self, "handle_scrollbar_changed")
+	max_scroll_length = scrollbar.max_value
+
+
+func handle_scrollbar_changed():
+	if max_scroll_length != scrollbar.max_value:
+		max_scroll_length = scrollbar.max_value
+		scroll.scroll_vertical = max_scroll_length
 
 
 func _on_Input_text_entered(new_text: String) -> void:
